610bdbbd2f84a8590d88d69f8722aa8ee90895fe
Tokusei Noborio <tokusei@nyampass.com>
2016-05-02 11:12:21 +0900

    Update robip pages

--- resources/public/js/jquery.mb.YTPlayer.js
+++ resources/public/js/jquery.mb.YTPlayer.js
@@ -15,6 +15,7 @@
  *  http://www.gnu.org/licenses/gpl.html
  *
  *  last modified: 30/08/13 23.31
+ *  e.g. modified: 2016/04/06
  *  *****************************************************************************
  */
 
@@ -1012,3 +1013,54 @@ function onYouTubePlayerAPIReady() {
 	jQuery.fn.manageYTPProgress = jQuery.mbYTPlayer.manageYTPProgress;
 
 })(jQuery);
+
+// $.browser.mobile
+(function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
+
+
+
+//extend checkmobile
+jQuery.fn.ifMobile = function(){
+
+//スマートフォンチェック
+//$.browserではなく、本プラグインにて実装しているメソッドです。
+if(!jQuery.browser.mobile)return false;
+
+//videoIDを取得
+//var videoID = this.getVideoID();
+
+//サムネイル取得
+//本来であれば,youtubeAPIを利用したほうが安全です。
+//srcをチェックして、サムネイルが存在しなければ、
+//代替画像を用意する等の処理は必要だと考えます。
+//e.g. var src = $('<img/>');src.src='THUMBNAIL.jpg';src.onload=function(){処理};
+//var src = 'http://img.youtube.com/vi/'+videoID+'/0.jpg';
+var src = 'http://img.youtube.com/vi/eQ-ru_8LbCc/0.jpg';
+//divオブジェクトを生成して、bodyに貼り付けます。
+var underlay = jQuery("<div/>").css({
+//ここは大事。divの背景としてサムネイル画像をセット
+"background":"url("+src+")",
+
+//ポジションとか大きさとか
+//下記を全て削除して、.YTPUnderlayにスタイルを付与刷るほうがいいかもです。
+//background-size:"cover"はサポートしているブラウザの確認が必要です。
+'background-size':"cover",
+"padding":0,
+"margin":0,
+"position": "absolute",
+"top": 0,
+"z-index":-1,
+"left": 0,
+"width": "100%",
+"height": "100%",
+})
+
+ //クラス付与
+//独自のクラス名なので、デザインを変更したい場合は、
+//.YTPUnderlayにスタイルを記述してください。
+//もちろんクラス名は任意につけていただけます。
+.addClass("YTPUnderlay");
+
+//body直下に挿入
+underlay.prependTo('body');
+}
